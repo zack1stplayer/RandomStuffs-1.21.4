7df7b1dd9317ad72e8a50fa5891680dcc266b0d1
zack1stplayer <zack1stplayer@gmail.com>
2025-01-15 15:48:03 -0800

    Reddite added/changed

--- src/main/java/cool/zack1stplayer/RandomStuffs/item/ModItems.java
+++ src/main/java/cool/zack1stplayer/RandomStuffs/item/ModItems.java
@@ -1,27 +1,29 @@
 package cool.zack1stplayer.RandomStuffs.item;
 
 import cool.zack1stplayer.RandomStuffs.RandomStuffsMain;
-import net.minecraft.core.Holder;
-import net.minecraft.core.component.DataComponentType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
-import javax.print.attribute.Attribute;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, RandomStuffsMain.MODID);
 
 
-    public static final RegistryObject<Item> MY_ITEM = ITEMS.register("my_item",
+    public static final RegistryObject<Item> REDDITE_LONGSWORD = ITEMS.register("reddite_longsword",
             () -> new Item(new Item.Properties()
-                    .setId(ITEMS.key("my_item"))
-//                    .attributes(ItemAttributeModifiers.builder().build())
+                    .setId(ITEMS.key("reddite_longsword"))
+                    .equippable(EquipmentSlot.byName("mainhand"))
+            )
+    );
+
+    public static final RegistryObject<Item> REDDITE_ITEM = ITEMS.register("reddite_item",
+            () -> new Item(new Item.Properties()
+                    .setId(ITEMS.key("reddite_item"))
             )
     );
 
