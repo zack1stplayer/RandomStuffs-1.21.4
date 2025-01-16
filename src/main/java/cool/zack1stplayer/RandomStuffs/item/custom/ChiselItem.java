1e49b41ea77a5e28221eda562625b1c4ab572b65
zack1stplayer <zack1stplayer@gmail.com>
2025-01-15 23:04:44 -0800

    Chisel & Growth Wand

---
+++ src/main/java/cool/zack1stplayer/RandomStuffs/item/custom/ChiselItem.java
@@ -0,0 +1,51 @@
+package cool.zack1stplayer.RandomStuffs.item.custom;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class ChiselItem extends Item {
+    private static final Map<Block, Block> CHISEL_MAP =
+            Map.ofEntries(
+                    Map.entry(Blocks.STONE, Blocks.STONE_BRICKS),
+                    Map.entry(Blocks.END_STONE, Blocks.END_STONE_BRICKS),
+                    Map.entry(Blocks.DEEPSLATE, Blocks.DEEPSLATE_BRICKS)
+            );
+
+
+    public ChiselItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(@NotNull UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        Block clickedBlock = level.getBlockState(pContext.getClickedPos()).getBlock();
+
+        if(CHISEL_MAP.containsKey(clickedBlock)) {
+            if(!level.isClientSide()) {
+                level.setBlockAndUpdate(pContext.getClickedPos(), CHISEL_MAP.get(clickedBlock).defaultBlockState());
+
+                pContext.getItemInHand().hurtAndBreak(1,
+                        ((ServerLevel) level),
+                        ((ServerPlayer) pContext.getPlayer()),
+                        item -> pContext.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND)
+                );
+                level.playSound(null, pContext.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
+            }
+        }
+
+        return super.useOn(pContext);
+    }
+}
